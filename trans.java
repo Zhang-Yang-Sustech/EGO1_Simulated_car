c9267ff4f21339f77b79526fc24ba0c016bedaa9
Zhang-Yang-Sustech <154614054+Zhang-Yang-Sustech@users.noreply.github.com>
2024-03-12 21:47:01 +0800

    Update trans.java

--- trans.java
+++ trans.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-
+//字模转化代码
 public class trans {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new FileReader("C:\\Users\\86178\\Desktop\\vivodo\\proje\\pepsi.TXT"));
