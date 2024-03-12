46dc919c0e4c6b3ab7d384f9d92eb31af7904ecc
Zhang-Yang-Sustech <154614054+Zhang-Yang-Sustech@users.noreply.github.com>
2024-03-12 21:44:01 +0800

    Add files via upload

---
+++ give.java
@@ -0,0 +1,20 @@
+package zimo;
+
+public class give {
+    public static void main(String[] args) {
+        int a = 0;
+        for (int i = 128; i < 192; i++) {
+            String ci = String.valueOf(i/64);
+            String ge ;
+            if (i<10) ge = "  ";
+            else ge ="";
+            if (a==64) a=0;
+            System.out.println("    char["+ge+String.valueOf(i)+"] <= {char"+ci+ci+"_0["+a+"],char"+ci+ci+"_1["+a+"],char"+ci+ci+"_2["+a+"],char"+ci+ci+"_3["+a+"],char"+ci+ci+"_4["+a+"],char"+ci+"["+a+"]};");
+            //System.out.println("    char["+ge+String.valueOf(i)+"] <= 512'h0;");
+            //System.out.println("    char22_0["+i+"] <= char22_0["+i+"];");
+            //System.out.println("    char22["+i+"] <= {char22_4["+i+"],char22_3["+i+"],char22_2["+i+"],char22_1["+i+"],char22_0["+i+"]};");
+            a++;
+
+        }
+    }
+}
---
+++ trans.java
@@ -0,0 +1,20 @@
+package zimo;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class trans {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new FileReader("C:\\Users\\86178\\Desktop\\vivodo\\proje\\pepsi.TXT"));
+        int i = 0;
+        String s;
+        while ((s=bufferedReader.readLine())!=null){
+            if (s.contains("zy")) {
+                System.out.println(s.replaceAll("char","      char").replaceAll("zy", String.valueOf(i)));
+                i++;
+            }
+        }
+    }
+}
